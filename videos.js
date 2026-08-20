2da6222658c99f6a9d2d725d5347e146fac01638
white3h <74317613+white3h@users.noreply.github.com>
2026-08-20 11:16:04 +0900

    Add bsVideos array with video data

---
+++ videos.js
@@ -0,0 +1,8 @@
+const bsVideos = [
+  { id: "LWMYh13gcSg", year: "2026", title: "2026 가풀지우 이달의바다 04월 인천삼목항" },
+  { id: "vCnjI6-VkAo", year: "2025", title: "2025 SEA너지 어린이 해양문화예술제" },
+  { id: "XGAeVdOKykc", year: "2024", title: "2024 경남사천바다 수중 폐어구 수거작전" },
+  { id: "ai90lUU6Sv4", year: "2023", title: "2023 한미 해병대 연합 호미곶 정화" },
+  { id: "GGKC_zZrdzw", year: "2022", title: "2022 침적쓰레기 제거 in BUSAN" },
+  { id: "nxKQoo4mh9A", year: "2022", title: "2022 부산 시민 1,000여명 플로깅" }
+];
