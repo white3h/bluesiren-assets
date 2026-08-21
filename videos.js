72c7e52ab02307657a464d8ffdadc3ca2c8554fb
white3h <74317613+white3h@users.noreply.github.com>
2026-08-21 16:49:27 +0900

    Fix typo in video title for 2026 entry

--- videos.js
+++ videos.js
@@ -1,5 +1,5 @@
 const bsVideos = [
-  { id: "LWMYh13gcSg", year: "2026", title: "2026 가풀지우 이달의바다 04월 인천삼목항" },
+  { id: "LWMYh13gcSg", year: "2026", title: "2026 가플지우 이달의바다 04월 인천삼목항" },
   { id: "vCnjI6-VkAo", year: "2025", title: "2025 SEA너지 어린이 해양문화예술제" },
   { id: "XGAeVdOKykc", year: "2024", title: "2024 경남사천바다 수중 폐어구 수거작전" },
   { id: "ai90lUU6Sv4", year: "2023", title: "2023 한미 해병대 연합 호미곶 정화" },
